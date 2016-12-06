a7e7ec2f90678b4bde0ea5f551459eecf954e8ee
imnottommy <fedscript@hotmail.com>
2016-12-06 00:45:37 -0500

    day 6

---
+++ 06/06b.tcl
@@ -0,0 +1,12 @@
+proc main {input} {
+	set length [string length [lindex $input 0]]
+	for {set i 0} {$i < $length} {incr i} {
+		set counter [dict create]
+		foreach str $input {
+			dict incr counter [string index $str $i]
+		}
+		puts [lindex [lsort -decreasing -integer -stride 2 -index 1 $counter] end-1]
+	}
+}
+
+main [split [read -nonewline stdin] "\n"]
