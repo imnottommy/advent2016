e47b0c80eb9e1010d2d01075012ede1c89f91183
imnottommy <fedscript@hotmail.com>
2016-12-13 03:08:10 -0500

    cleanup

--- 13/13.tcl
+++ 13/13.tcl
@@ -1,66 +1,32 @@
-proc dec2bin i {
-    set res {} 
-    while {$i>0} {
-        set res [expr {$i%2}]$res
-        set i [expr {$i/2}]
-    }
-    if {$res == {}} {set res 0}
-    return $res
+set max 100
+proc d2b {decimal} {
+	binary scan [binary format {I} $decimal] {B*} binary
+
+	return $binary
 }
 
-proc iswall {x y} {
+proc iswall {x y {code {1362}}} {
 	if {$x < 0 || $y < 0} {
 		return 1
 	}
 
-	set input 1362
 	if {![info exists map($x,$y)]} {
-		set map($x,$y) [expr [llength [lsearch -inline -all [split [dec2bin [expr ($x*$x + 3*$x + 2*$x*$y + $y + $y*$y) + $input]] {}] {1}]] % 2]
-#		puts [dec2bin [expr ($x*$x + 3*$x + 2*$x*$y + $y + $y*$y) + $input]]
-#		puts [expr ($x*$x + 3*$x + 2*$x*$y + $y + $y*$y) + $input]
+		set map($x,$y) [expr [llength [lsearch -inline -all [split [d2b [expr ($x*$x + 3*$x + 2*$x*$y + $y + $y*$y) + $code]] {}] {1}]] % 2]
 	}
 
 	return $map($x,$y)
 }
 
-proc print {moves} {
-global what
-	foreach move $moves {
-		lassign $move x y
-		set what($x,$y) 0
-	}
-
-	for {set i 0} {$i < 80} {incr i} {
-		for {set j 0} {$j < 100} {incr j} {
-			set x {.}
-			if {[iswall $j $i]} {
-				set x {#}
-			}
-			if {[info exists what($j,$i)]} {
-				set x 0
-			}		
-			puts -nonewline "$x "
-		}
-		puts -nonewline "\n"
-	}
-
-}
-
 proc valid_moves {x y moves} {
+	global max
 	if {$x == 31 && $y == 39} {
 		puts [expr [llength $moves] -1]
-#		print $moves
 	}
 
-	set original $moves
-	if {[llength $moves] < 100} {
+	if {[llength $moves] < $max} {
 		foreach move [list [list $x [expr $y+1]] [list $x [expr $y-1]] [list [expr $x+1] $y] [list [expr $x-1] $y]] {
-			set moves $original
-			if {![iswall {*}$move]} {
-				if {[lsearch $moves $move] == -1} {
-					lappend moves $move
-					valid_moves {*}$move $moves
-				}
+			if {![iswall {*}$move] && [lsearch $moves $move] == -1} {
+				valid_moves {*}$move [list {*}$moves $move]
 			}
 		}
 	}
